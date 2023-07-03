f4b50a5697e5222e7a2892f26fde91f569e58948
Eric Ranner <eranner@Erics-MacBook-Pro.local>
2023-07-02 21:30:13 -0400

    change to home page

--- BadBank/public/home.js
+++ BadBank/public/home.js
@@ -1,5 +1,30 @@
 function Home(){
-    
+  const useEffect = React.useEffect
+  const {userName, setUsername }= React.useContext(UserContext)
+  const { userBalance, setUserBalance }= React.useContext(UserContext)
+  const { isLoggedIn, setIsLoggedIn }= React.useContext(UserContext)
+
+  function checkIfLoggedIn(){
+      if(localStorage.getItem("token")){
+          // console.log("yes")
+          // setIsLoggedIn(true)
+          // setShow(false)
+          const firstLoginCheck = JSON.parse(localStorage.getItem("loggedIn"))
+          setIsLoggedIn(firstLoginCheck)
+          // console.log(userName)
+      }
+  }
+  useEffect(() => {
+      checkIfLoggedIn();
+      if(isLoggedIn == true){
+        const newUserName = window.localStorage.getItem('token')
+        const parsedUserInfo = JSON.parse(newUserName)
+        // console.log(parsedUserInfo.userName)
+        setUsername(parsedUserInfo.userName)
+        setUserBalance(parsedUserInfo.userBalance.toFixed(2))
+        // console.log(userName)
+      }
+    }, [isLoggedIn]);
     return(
       <Card 
         bgcolor='danger'
